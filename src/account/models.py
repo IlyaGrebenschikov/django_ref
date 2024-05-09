5d81088de0e7c25d7c79606e5226715a5a8a4902
IlyaGrebenschikov <ilushagr22@gmail.com>
2024-05-10 01:25:10 +0400

    user model

--- src/account/models.py
+++ src/account/models.py
@@ -1,3 +1,21 @@
 from django.db import models
 
 # Create your models here.
+from django.contrib.auth.models import User
+from django.urls import reverse
+from django.db import models
+
+
+class MyUser(User):
+    avatar = models.ImageField(default='default.jpg', upload_to='profile_images')
+    referred_by = models.CharField()
+
+    class Meta:
+        verbose_name = 'Пользователь'
+        verbose_name_plural = 'Пользователи'
+
+    def __str__(self):
+        return f'Имя: {self.username}, email: {self.email}'
+
+    def get_absolute_url(self):
+        return reverse("user-details", kwargs={"pk": self.pk})
