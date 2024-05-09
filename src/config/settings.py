70763a778b0f9cbd89ab414c3c36f4c8632966a0
IlyaGrebenschikov <ilushagr22@gmail.com>
2024-05-10 01:20:24 +0400

    register main app and change lang

--- src/config/settings.py
+++ src/config/settings.py
@@ -46,6 +46,10 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    # local
+    'main.apps.MainConfig',
+
 ]
 
 MIDDLEWARE = [
@@ -116,7 +120,7 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/5.0/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'ru-ru'
 
 TIME_ZONE = 'UTC'
 
