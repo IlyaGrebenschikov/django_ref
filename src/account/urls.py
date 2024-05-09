74d9645ad95e128bb652b17e2cca4ea09094c0c2
IlyaGrebenschikov <ilushagr22@gmail.com>
2024-05-10 01:58:39 +0400

    account urls

---
+++ src/account/urls.py
@@ -0,0 +1,11 @@
+from django.urls import path
+
+from . import views
+
+
+urlpatterns = [
+    path('register/', views.UserRegistrationView.as_view(), name='register'),
+    path('me/<int:pk>/', views.UserDetailView.as_view(), name='user-details'),
+    path('login/', views.MyLoginView.as_view(), name='login'),
+    path('logout/', views.MyLogoutView.as_view(), name='logout'),
+]
--- src/config/urls.py
+++ src/config/urls.py
@@ -20,4 +20,5 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('main.urls')),
+    path('account/', include('account.urls')),
 ]
