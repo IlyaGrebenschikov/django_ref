113f6227bbf62329a49e6da77d27c9297ec58cea
IlyaGrebenschikov <ilushagr22@gmail.com>
2024-05-10 02:03:07 +0400

    media and account settings

--- src/config/settings.py
+++ src/config/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.0/ref/settings/
 """
 
+import os
 from pathlib import Path
 
 from .env_settings import (
@@ -139,3 +140,12 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+# Media
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+MEDIA_URL = '/media/'
+
+# Account
+LOGIN_REDIRECT_URL = '/'
+LOGIN_URL = 'accounts/login'
+LOGOUT_REDIRECT_URL = '/home'
