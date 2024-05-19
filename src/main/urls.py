b673761dff2dc725825da378d8157e9c4a30a661
IlyaGrebenschikov <ilushagr22@gmail.com>
2024-05-19 16:57:21 +0400

    rework main view

--- src/main/urls.py
+++ src/main/urls.py
@@ -1,8 +1,7 @@
 from django.urls import path
-
-from . import views
+from django.views.generic import TemplateView
 
 
 urlpatterns = [
-    path('', views.index, name='home'),
+    path('', TemplateView.as_view(template_name='main/base.html'), name='home'),
 ]
--- src/main/views.py
+++ src/main/views.py
@@ -1,7 +1,3 @@
 from django.shortcuts import render
 
 # Create your views here.
-
-
-def index(request):
-    return render(request, 'main/base.html')
