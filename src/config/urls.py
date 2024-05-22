d39ccef490d147077c0e90692890c1ef7dbfeeb9
IlyaGrebenschikov <ilushagr22@gmail.com>
2024-05-22 21:28:56 +0400

    add static data

--- src/config/urls.py
+++ src/config/urls.py
@@ -16,10 +16,14 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('main.urls')),
     path('account/', include('account.urls')),
     path('referral_system/', include('referral_system.urls')),
-]
+] + static(
+    settings.STATIC_URL,
+    document_root=settings.STATIC_ROOT)
