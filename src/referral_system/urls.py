642d6f473fdbb2b0bf84ee1fa725c6ace0ac7a37
IlyaGrebenschikov <ilushagr22@gmail.com>
2024-05-22 17:23:07 +0400

    add urls (delete_link, show_referrers, add_link)

--- src/referral_system/urls.py
+++ src/referral_system/urls.py
@@ -5,4 +5,7 @@ from . import views
 
 urlpatterns = [
     path('create_link/', views.create_referral_view, name='create_link'),
+    path('delete_link/', views.delete_referral_view, name='delete_link'),
+    path('show_referrers/', views.show_referrers_view, name='show_referrers'),
+    path('add_link/', views.add_referral_view, name='add_link'),
 ]
