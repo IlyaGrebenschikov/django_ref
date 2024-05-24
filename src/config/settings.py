fd3c4cde8023ccd4bbb303fd599b608c2bfba55e
IlyaGrebenschikov <ilushagr22@gmail.com>
2024-05-24 14:38:47 +0300

    add allowed hosts

--- src/config/settings.py
+++ src/config/settings.py
@@ -37,7 +37,7 @@ SECRET_KEY = ENV_SECRET_KEY
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['127.0.0.1', '0.0.0.0']
 
 
 # Application definition
